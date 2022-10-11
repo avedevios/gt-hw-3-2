14f513007d62b67ff9d295dc3ae2e3d4f88d66ce
avedevios <106875633+avedevios@users.noreply.github.com>
2022-10-11 11:56:35 +0600

    Initial Commit

---
+++ gt-hw-3-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gt-hw-3-2
+//
+//  Created by ake11a on 2022-10-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
